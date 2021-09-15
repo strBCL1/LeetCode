1f49eb8facfc0b8b8455e503242364a15d3d0850
Artem Popov <70245014+strBCL1@users.noreply.github.com>
2021-09-15 21:00:54 +0200

    Update Valid Parantheses.cpp

--- Easy/Valid Parantheses/Valid Parantheses.cpp
+++ Easy/Valid Parantheses/Valid Parantheses.cpp
@@ -4,9 +4,8 @@ public:
     bool isValid(string& s) {
         stack<char> st; //Declare stack to store parantheses;
         
-        //For each paranthesis in stirng:
+        //For each paranthesis in string:
         for (const char& c : s) {
-            
             //If stack isn't empty and its top paranthesis is a pair to current paranthesis --> erase top paranthesis from stack;
             //Else push current paranthesis to stack;
             if (!st.empty() && 
