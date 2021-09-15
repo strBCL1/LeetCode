46605c34ceb065c75b2742b1a1bdb1810baee9c7
Artem Popov <70245014+strBCL1@users.noreply.github.com>
2021-09-15 20:57:35 +0200

    Create Valie Parantheses.cpp

---
+++ Easy/Valie Parantheses/Valie Parantheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string& s) {
+        stack<char> st; //Declare stack to store parantheses;
+        
+        //For each paranthesis in stirng:
+        for (const char& c : s) {
+            
+            //If stack isn't empty and its top paranthesis is a pair to current paranthesis --> erase top paranthesis from stack;
+            //Else push current paranthesis to stack;
+            if (!st.empty() && 
+               ( (c == ']' && st.top() == '[') ||
+                 (c == ')' && st.top() == '(') ||
+                 (c == '}' && st.top() == '{')) )
+                st.pop();
+            else 
+                st.push(c);
+        }
+        
+        //If stack is empty, it means each paranthesis has its pair in string, so the stack would always be empty;
+        return st.empty();
+    }
+};
